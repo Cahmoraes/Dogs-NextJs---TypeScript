257371e02ef919cc939f8acf9691b759ad356bcc
caique.moraes <ike.moraes93@gmail.com>
2023-03-08 21:07:31 -0300

    Implementado Image Preview

--- src/pages/conta/postar/index.page.tsx
+++ src/pages/conta/postar/index.page.tsx
@@ -1,12 +1,14 @@
 import { withClientSession } from '@/utils/withClientSession'
 import { useForm } from 'react-hook-form'
 import { AccountLayout } from '../layout'
-import { FormPhotoPost, UserPhotoPostContainer } from './styles'
+import { FormPhotoPost, ImagePreview, UserPhotoPostContainer } from './styles'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Input } from '@/components/forms/Input'
 import { Button } from '@/components/forms/Button'
 import { ApiService } from '@/services/ApiService'
+import { ChangeEvent, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const FormUserPhotoPostSchema = z.object({
   nome: z.string().min(1, 'Nome é obrigatório'),
@@ -18,17 +20,22 @@ const FormUserPhotoPostSchema = z.object({
 type FormUserPhotoPostSchemaType = z.infer<typeof FormUserPhotoPostSchema>
 
 function UserPhotoPost() {
+  const [imagePreview, setImagePreview] = useState<string | null>(null)
+  const router = useRouter()
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<FormUserPhotoPostSchemaType>({
     resolver: zodResolver(FormUserPhotoPostSchema),
   })
 
   console.log(errors)
 
-  async function handleSubmitPhotoPost(data: FormUserPhotoPostSchemaType) {
+  async function handleSubmitPhotoPost(
+    data: FormUserPhotoPostSchemaType,
+  ): Promise<void> {
     try {
       const { nome, peso, idade, img } = data
       const formData = new FormData()
@@ -39,32 +46,69 @@ function UserPhotoPost() {
       formData.append('img', (img as FileList)[0])
 
       await ApiService.photo.post(formData)
+      await router.push('/conta')
     } catch (error) {
       console.log(error)
     }
+  }
+
+  function handleImagePreview({ target }: ChangeEvent<HTMLInputElement>) {
+    console.log(target)
+    if (target?.files?.length! < 1) return
+    const preview = URL.createObjectURL(target.files![0])
+    setImagePreview(preview)
+  }
+
+  function renderImagePreview() {
+    return (
+      imagePreview && (
+        <div>
+          <ImagePreview style={{ backgroundImage: `url(${imagePreview})` }} />
+        </div>
+      )
+    )
+  }
+
+  function rendeSubmitButton() {
+    return (
+      <Button disabled={isSubmitting}>
+        {isSubmitting ? 'Carregando...' : 'Enviar'}
+      </Button>
+    )
   }
 
   return (
     <AccountLayout>
       <UserPhotoPostContainer className="animeLeft">
         <FormPhotoPost onSubmit={handleSubmit(handleSubmitPhotoPost)}>
-          <Input label="Nome" {...register('nome')} />
+          <Input label="Nome" error={errors.nome} {...register('nome')} />
 
           <Input
             label="Peso"
             type="number"
+            error={errors.peso}
             {...register('peso', { valueAsNumber: true })}
           />
 
           <Input
             label="Idade"
             type="number"
+            error={errors.idade}
             {...register('idade', { valueAsNumber: true })}
           />
 
-          <input type="file" id="img" {...register('img')} />
-          <Button>Enviar</Button>
+          <input
+            type="file"
+            id="img"
+            className="file"
+            {...register('img')}
+            onChange={handleImagePreview}
+          />
+
+          {rendeSubmitButton()}
         </FormPhotoPost>
+
+        {renderImagePreview()}
       </UserPhotoPostContainer>
     </AccountLayout>
   )
--- src/pages/conta/postar/styles.ts
+++ src/pages/conta/postar/styles.ts
@@ -1,5 +1,28 @@
 import { styled } from '@/styles/stitches.config'
 
-export const UserPhotoPostContainer = styled('section', {})
+export const UserPhotoPostContainer = styled('section', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gap: '2rem',
+  marginBottom: '2rem',
+
+  '& .file': {
+    marginBottom: '1rem',
+  },
+})
 
 export const FormPhotoPost = styled('form', {})
+
+export const ImagePreview = styled('div', {
+  borderRadius: '1rem',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+
+  '&::after': {
+    content: '',
+    display: 'block',
+    height: 0,
+    paddingBottom: '100%',
+  },
+})

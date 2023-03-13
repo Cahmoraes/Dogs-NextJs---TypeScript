05f99243643a7204dcc3e5eb2c8439f4ce5f7d0c
caique.moraes <ike.moraes93@gmail.com>
2023-03-13 13:28:37 -0300

    feature: página de erro

---
+++ src/pages/404.page.tsx
@@ -0,0 +1,10 @@
+import { Title } from '@/components/Title'
+
+export default function Custom404() {
+  return (
+    <div className="container main-container">
+      <Title>Erro: 404</Title>
+      <p>Página não encontrada</p>
+    </div>
+  )
+}
